5f2cd74436c8266f7ca9650bf78c2690a360783f
andyadc <andaicheng@gmail.com>
2023-07-21 17:23:42 +0800

    lottery

--- lottery-domain/src/main/java/com/andyadc/lottery/domain/rule/annotation/Strategy.java
+++ lottery-domain/src/main/java/com/andyadc/lottery/domain/rule/annotation/Strategy.java
@@ -1,11 +1,17 @@
 package com.andyadc.lottery.domain.rule.annotation;
 
+import com.andyadc.lottery.common.Constants;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 抽奖策略模型注解
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Strategy {
+    Constants.StrategyMode mode();
 }
