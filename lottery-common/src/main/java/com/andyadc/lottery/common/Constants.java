7d64a369b30887c9f41fedf852e69c3e0d07fd24
andyadc <andaicheng@gmail.com>
2023-07-21 17:21:57 +0800

    lottery

--- lottery-common/src/main/java/com/andyadc/lottery/common/Constants.java
+++ lottery-common/src/main/java/com/andyadc/lottery/common/Constants.java
@@ -24,6 +24,80 @@ public class Constants {
     }
 
     /**
+     * 中奖状态：0未中奖、1已中奖、2兜底奖
+     */
+    public enum DrawState {
+
+        /**
+         * 已中奖
+         */
+        SUCCESS(1, "已中奖"),
+
+        /**
+         * 未中奖
+         */
+        FAIL(0, "未中奖"),
+
+        /**
+         * 兜底奖
+         */
+        COVER(2, "兜底奖");
+
+        Integer code;
+        String desc;
+
+        DrawState(Integer code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public Integer code() {
+            return code;
+        }
+
+        public String desc() {
+            return desc;
+        }
+
+    }
+
+    /**
+     * 抽奖策略模式：总体概率、单项概率
+     * 场景：两种抽奖算法描述，场景A20%、B30%、C50%
+     * 单项概率：如果A奖品抽空后，B和C保持目前中奖概率，用户抽奖扔有20%中为A，因A库存抽空则结果展示为未中奖。为了运营成本，通常这种情况的使用的比较多
+     * 总体概率：如果A奖品抽空后，B和C奖品的概率按照 3:5 均分，相当于B奖品中奖概率由 0.3 升为 0.375
+     */
+    public enum StrategyMode {
+
+        /**
+         * 单项概率：如果A奖品抽空后，B和C保持目前中奖概率，用户抽奖扔有20%中为A，因A库存抽空则结果展示为未中奖。为了运营成本，通常这种情况的使用的比较多
+         */
+        SINGLE(1, "单项概率"),
+
+        /**
+         * 总体概率：如果A奖品抽空后，B和C奖品的概率按照 3:5 均分，相当于B奖品中奖概率由 0.3 升为 0.375
+         */
+        ENTIRETY(2, "总体概率");
+
+        Integer code;
+        String desc;
+
+        StrategyMode(Integer code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public Integer code() {
+            return code;
+        }
+
+        public String desc() {
+            return desc;
+        }
+
+    }
+
+    /**
      * 缓存 Key
      */
     public static final class RedisKey {
